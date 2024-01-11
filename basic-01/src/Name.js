b3b18290de469bca31a8cf767554224b2a83e497
AshutoshSharma123 <aashusharma375@gmail.com>
2024-01-11 22:24:59 +0530

    checked how file changes  works in react app

---
+++ basic-01/src/Name.js
@@ -0,0 +1,6 @@
+function name() {
+    return (
+        <h2>Hello from name which is in Youtube.js</h2>
+    )
+}
+export default name;
---
+++ basic-01/src/Youtube.js
@@ -0,0 +1,9 @@
+import Name from "./Name"
+function Youtube() {
+    return (
+        <>        <h1> Hi from Youtube.js</h1>
+            <Name />
+        </>
+    )
+}
+export default Youtube
