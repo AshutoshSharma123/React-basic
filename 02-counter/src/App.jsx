d07ed7fcfb53c8b97940ad718fc414cda755b903
AshutoshSharma123 <aashusharma375@gmail.com>
2024-01-11 23:58:45 +0530

    added a project i.e. 02- counter where the efficient use of state/callback is used

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -1,12 +1,23 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let counter = 15
+
+  const [counter, setCounter] = useState(15)
+
 
   const addValue = () => {
-    counter += 1
-    console.log(counter)
+
+    setCounter((previousCounter) => previousCounter + 1)
+    setCounter((previousCounter) => previousCounter + 2)
+    setCounter((previousCounter) => previousCounter + 1)
+    setCounter((previousCounter) => previousCounter + 1)
+
+
+  }
+  const removevalue = () => {
+    setCounter(counter - 1)
   }
 
   return (
@@ -14,10 +25,11 @@ function App() {
       <h1>React with Ashutosh Sharma</h1>
       <h2>Counter Value:{counter}</h2>
       <button onClick={addValue}> Add value</button>
-      <button>Remove value</button>
+      <button onClick={removevalue}>Remove value</button>
       <p>footer:{counter}</p>
     </>
   )
+
 }
 
 export default App
